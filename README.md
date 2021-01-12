0dcc569f8c5b013688aff40c00450d2d396c8cb1
aisnote <aisnote@gmail.com>
2021-01-12 08:48:51 -0800

    add screen shot, release dev-version

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # lomo-importer
-Batch import tool like: import Google Photos to Lomorage
+**Batch import tool like: import Google Photos to Lomorage**
+
+## Eg: how to import from Google Photo
+### **1. download your google photo from takeout.google.com**
+### **2. login to Lomorage server**
+   
+ ![Login](./images/login.png)
+
+### **3. add all zip files to lomoimpt.exe(.app) and click "2.Start import" button**
+
+![Login](./images/import.png)
+
+
+# Note:
+This is the **dev-version**, not the final release.
+please create new issue on github for new feature or bugs.
+
+support at lomorage.com is the email.
+
+[slack channel](https://join.slack.com/t/lomorage/shared_invite/enQtODc4MTE5ODQzNzkyLTRlY2U4MTQ1YjczYjBhMDcwMmExYTUxNTg2NTE5YmRkZjg2ZWQwZjg1MjEwMjQzZWVjMmEwZjk3ZGIyODY4ODM)
---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying lomoimpt.zip to GitHub...\033[0m"
+ 
+### mm/dd/yyyy ###
+now=$(date +'%Y-%m-%d_%H-%M-%S')
+echo $now
+
+rm -fr *.zip
+
+### $1
+orig_apk_path=$1
+echo "orig_apk_path="$orig_apk_path
+if [[ "$orig_apk_path" == "" ]]; then
+    orig_apk_path='/h/myproject/lomoware/lomo-android/app/release/'
+fi
+
+### $2
+orig_apk_file=$2
+echo "orig_apk_file="$orig_apk_file
+if [[ "$orig_apk_file" == "" ]]; then
+    orig_apk_file="com.lomoware.lomorage-v9(0.72)-release"
+fi
+
+
+echo $orig_apk_file
+
+orig_apk_file_path=$orig_apk_path$orig_apk_file'.zip'
+
+echo $orig_apk_file_path
+
+cp $orig_apk_file_path ./
+
+release_apk_file=$orig_apk_file$now.zip
+
+echo $release_apk_file
+
+mv "./"$orig_apk_file".zip" "./"$release_apk_file
+
+
+
+tag=$now
+hub release delete $now
+hub release create -a $release_apk_file -m "latest release $now" $now
